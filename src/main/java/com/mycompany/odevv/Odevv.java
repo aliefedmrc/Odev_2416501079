0e9cec632a73b13c0154ed1fe63085f00759150f
aliefedmrc <aliefedemirci582005@gmail.com>
2026-05-30 00:04:20 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/odevv/Odevv.java
@@ -0,0 +1,14 @@
+/*
+ * Bu sınıf kullanılmıyor. Ana sınıf: Odev_2416501079
+ */
+package com.mycompany.odevv;
+
+/**
+ *
+ * @author Ali Efe
+ */
+public class Odevv {
+    public static void main(String[] args) {
+        Odev_2416501079.main(args);
+    }
+}
